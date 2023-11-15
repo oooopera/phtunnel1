b8770a29368e1ac491085bd54dfa2d2a72001455
oooopera <sweetbox-sx@qq.com>
2023-11-15 16:10:46 +0800

    new

--- luci-app-phtunnel/luasrc/controller/oray/phtunnel.lua
+++ luci-app-phtunnel/luasrc/controller/oray/phtunnel.lua
@@ -4,7 +4,7 @@ function index()
 	entry({"admin", "services", "phtunnel"}, alias("admin", "services", "phtunnel", "setup"), _("Phtunnel"))
 	entry({"admin", "services", "phtunnel", "setup"}, cbi("oray/phtunnel_setup"), _("Setup"), 1).leaf = true
 	entry({"admin", "services", "phtunnel", "status"}, template("oray/phtunnel_status"), _("Status"), 2).leaf = true
-     entry({"admin", "oray", "phtunnel", "log"}, template("oray/phtunnel_log"), _("Log"), 3).leaf = true
+     entry({"admin", "services", "phtunnel", "log"}, template("oray/phtunnel_log"), _("Log"), 3).leaf = true
 
 	local node = entry({"admin", "services", "phtunnel", "inner_status"}, template("oray/phtunnel_inner_status"), nil, 4)
 	node.leaf = true
