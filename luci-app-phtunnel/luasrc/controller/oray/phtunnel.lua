cd2237867cb4fca474d458a9d17abe676c7933cb
oooopera <sweetbox-sx@qq.com>
2023-11-18 14:15:27 +0800

    new

--- luci-app-phtunnel/luasrc/controller/oray/phtunnel.lua
+++ luci-app-phtunnel/luasrc/controller/oray/phtunnel.lua
@@ -5,13 +5,13 @@ function index()
 	entry({"admin", "services", "phtunnel", "setup"}, cbi("oray/phtunnel_setup"), _("Setup"), 1).leaf = true
 	entry({"admin", "services", "phtunnel", "status"}, template("oray/phtunnel_status"), _("Status"), 2).leaf = true
      entry({"admin", "services", "phtunnel", "log"}, template("oray/phtunnel_log"), _("Log"), 3).leaf = true
-     entry({"admin", "services", "phtunnel", "inner_status"}, template("oray/phtunnel_inner_status"), _("inner_status"), 4).leaf = true
-     entry({"admin", "services", "phtunnel", "log_off"}, template("oray/phtunnel_log_off"), _("log_off"), 5).leaf = true
---	local node = entry({"admin", "services", "phtunnel", "inner_status"}, template("oray/phtunnel_inner_status"), nil, 4)
---	node.leaf = true
---	node.hidden = false
---
---	node = entry({"admin", "services", "phtunnel", "log_off"}, template("oray/phtunnel_log_off"), nil, 5)
---	node.leaf = true
---	node.hidden = false
+--     entry({"admin", "services", "phtunnel", "inner_status"}, template("oray/phtunnel_inner_status"), _("inner_status"), 4).leaf = true
+--     entry({"admin", "services", "phtunnel", "log_off"}, template("oray/phtunnel_log_off"), _("log_off"), 5).leaf = true
+	local node = entry({"admin", "services", "phtunnel", "inner_status"}, template("oray/phtunnel_inner_status"), nil, 4)
+	node.leaf = true
+	node.hidden = true
+
+	node = entry({"admin", "services", "phtunnel", "log_off"}, template("oray/phtunnel_log_off"), nil, 5)
+	node.leaf = true
+	node.hidden = true
 end
